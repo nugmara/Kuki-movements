e00b8acf56850007ce58467ce20e971f2416e6c8
Dagmara Grabowska <dagmarag0@gmail.com>
2023-03-22 14:31:11 +0100

    improvement in the movement of the player

--- script.js
+++ script.js
@@ -7,13 +7,43 @@ const playerImage = new Image()
 playerImage.src = "./images/shadow_dog.png"
 const spriteWidth = 575;
 const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: "idle",
+        frames: 7,
+    },
+    {
+        name: "jump",
+        frames: 7
+    }
+];
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for(let j = 0; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY})
+    }
+    spriteAnimations[state.name] = frames;
+})
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
+    frameX = spriteWidth * position;
     // ctx.fillRect(50,50, 100, 100)
     // ctx.drawImage(image, sx, sy, sw, sh, the last four arguments will tell js where on our
     // destination canvas we want to draw just that cropped out part onto => dx, dy, dw, dh)
-    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+   
+    gameFrame++
     requestAnimationFrame(animate)
 }
 animate();
