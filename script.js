fdc19d96e40031a3376e7694f28c9670032874eb
Dagmara Grabowska <dagmarag0@gmail.com>
2023-03-22 14:53:23 +0100

    movements done

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let playerState = "run"
+const dropdown = document.getElementById("animations")
+dropdown.addEventListener("change", function(e){
+    playerState = e.target.value
+})
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 600);
@@ -7,8 +12,6 @@ const playerImage = new Image()
 playerImage.src = "./images/shadow_dog.png"
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
 const spriteAnimations = [];
@@ -20,6 +23,38 @@ const animationStates = [
     {
         name: "jump",
         frames: 7
+    },
+    {
+        name: "fall",
+        frames: 7
+    },
+    {
+        name: "run",
+        frames: 9
+    },
+    {
+        name: "dizzy",
+        frames: 11
+    },
+    {
+        name: "sit",
+        frames: 5
+    },
+    {
+        name: "roll",
+        frames: 7
+    },
+    {
+        name: "bite",
+        frames: 7
+    },
+    {
+        name: "ko",
+        frames: 12
+    },
+    {
+        name: "getHit",
+        frames: 4
     }
 ];
 animationStates.forEach((state, index) => {
@@ -33,15 +68,17 @@ animationStates.forEach((state, index) => {
     }
     spriteAnimations[state.name] = frames;
 })
+console.log(spriteAnimations)
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    let position = Math.floor(gameFrame/staggerFrames) % 6;
-    frameX = spriteWidth * position;
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y
     // ctx.fillRect(50,50, 100, 100)
     // ctx.drawImage(image, sx, sy, sw, sh, the last four arguments will tell js where on our
     // destination canvas we want to draw just that cropped out part onto => dx, dy, dw, dh)
-    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
    
     gameFrame++
     requestAnimationFrame(animate)
